bec41245c0f02a2f6a19471204ad2ebc972b7cb3
NathanMB77 <160797692+NathanMB77@users.noreply.github.com>
2024-03-11 23:45:51 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let word1 = ["A", "B", "C"];
+  let word2 = ["D", "E", "F"];
+  let word3 = ["G", "H", "I"];
+
+  let count = 1;
+  let fullstring = "";
+  for (let i = 0; i < word1.length; i++) {
+    for (let j = 0; j < word2.length; j++) {
+      for (let k = 0; k < word3.length; k++) {
+        console.log(
+          count.toString() + ": " + word1[i] + word2[j] + word3[k] + ".com"
+        );
+        fullstring =
+          fullstring +
+          count.toString() +
+          ": " +
+          word1[i] +
+          word2[j] +
+          word3[k] +
+          ".com" +
+          "<br />";
+        count++;
+      }
+    }
+  }
+  document.getElementById("fullstring").innerHTML = fullstring;
 };
